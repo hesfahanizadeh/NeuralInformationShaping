264be1ac325cd46a2448456cbe1e5194f766ee8e
kaan-kale <hkaankale1@gmail.com>
2024-04-08 06:46:30 +0200

    Fixed dataclasses

--- TexShape/src/utils/data_structures.py
+++ TexShape/src/utils/data_structures.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 
+
 @dataclass
 class MINE_Params:
     utility_stats_network_model_name: str
@@ -12,23 +13,41 @@ class MINE_Params:
     mine_batch_size: int = -1
     mine_epochs_privacy: int = 2000
     mine_epochs_utility: int = 2000
-    # Use default factory function to avoid mutable default arguments
-    # if string passed then convert to Path object
-    utility_stats_network_model_path: Path = field(default_factory=Path)
-    privacy_stats_network_model_path: Path = field(default_factory=Path)
-    
+    utility_stats_network_model_path: Path = field(default="")
+    privacy_stats_network_model_path: Path = field(default="")
+
+    def __post_init__(self):
+        if self.use_prev_epochs_mi_model:
+            self.utility_stats_network_model_path = Path(
+                self.utility_stats_network_model_path
+            )
+            self.privacy_stats_network_model_path = Path(
+                self.privacy_stats_network_model_path
+            )
+
+
 @dataclass
 class EncoderParams:
     encoder_model_name: str
     encoder_model_params: dict
     num_enc_epochs: int = 10
-    enc_save_dir_path: Path = field(default_factory=Path)
+    enc_save_dir_path: Path = field(default="")
+
+    def __post_init__(self):
+        if self.enc_save_dir_path:
+            self.enc_save_dir_path = Path(self.enc_save_dir_path)
+
 
 @dataclass
 class LogParams:
-    log_dir_path: Path = field(default_factory=Path)
-    log_file_path: Path = field(default_factory=Path)
-    
+    log_dir_path: Path = field(default="")
+    log_file_path: Path = field(default="")
+
+    def __post_init__(self):
+        self.log_dir_path = Path(self.log_dir_path)
+        self.log_file_path = Path(self.log_file_path)
+
+
 @dataclass
 class ExperimentParams:
     dataset_name: str
@@ -38,10 +57,10 @@ class ExperimentParams:
     encoder_params: EncoderParams
     log_params: LogParams
     experiment_date: str
-    
-    beta: float = 0.2   
+
+    beta: float = 0.2
     combination_type: str = "premise_only"
-    
+
     @property
     def experiment_name(self) -> str:
         return f"{self.dataset_name}_{self.experiment_type}_{self.experiment_date}"
